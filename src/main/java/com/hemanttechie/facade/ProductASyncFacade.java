2aa9c9aa980a5710ac7518780aa4b886b57e4efd
Hemant Sahu <hemanttechie22@gmail.com>
2024-11-13 13:23:09 +0530

    update the code

--- src/main/java/com/hemanttechie/facade/ProductASyncFacade.java
+++ src/main/java/com/hemanttechie/facade/ProductASyncFacade.java
@@ -1,92 +1,9 @@
 package com.hemanttechie.facade;
 
 import com.hemanttechie.dto.ProductDetailDTO;
-import com.hemanttechie.entity.Inventory;
-import com.hemanttechie.entity.Price;
-import com.hemanttechie.entity.Product;
-import com.hemanttechie.service.InventoryService;
-import com.hemanttechie.service.PriceService;
-import com.hemanttechie.service.ProductService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
-import java.util.concurrent.CompletableFuture;
+public interface ProductASyncFacade {
 
-@Service
-@Slf4j
-@RequiredArgsConstructor
-public class ProductASyncFacade {
-
-
-    private final ProductService productService;
-
-    private final InventoryService inventoryService;
-
-    private final PriceService priceService;
-
-    private CompletableFuture<Product> getProductById(long productId) {
-        return CompletableFuture
-                .supplyAsync(() -> productService.findById(productId));
-    }
-
-    private CompletableFuture<Price> getPriceByProductById(long productId) {
-        return CompletableFuture
-                .supplyAsync(() -> priceService.getPriceByProductId(productId));
-    }
-
-    private CompletableFuture<Inventory> getInventoryByProductId(long productId) {
-        return CompletableFuture
-                .supplyAsync(() -> inventoryService.getInventoryByProductId(productId));
-    }
-
-
-  /*  public ProductDetailDTO getProductDetails(long productId) {
-
-        //fetch all async
-        CompletableFuture<Product> productFuture = getProductById(productId);
-        CompletableFuture<Price> priceFuture = getPriceByProductById(productId);
-        CompletableFuture<Inventory> inventoryFuture = getInventoryByProductId(productId);
-
-        //wait for all futures to complete
-        CompletableFuture.allOf(priceFuture, productFuture, inventoryFuture);
-
-        //combine the result
-        Product product = productFuture.join();
-        Price price = priceFuture.join();
-        Inventory inventory = inventoryFuture.join();
-
-        //build and return
-
-        return new ProductDetailDTO(productId, product.getCategory().getName(),
-                product.getName(), product.getDescription(),
-                inventory.getAvailableQuantity(), price.getPrice(),
-                inventory.getStatus());
-    }*/
-
-    public ProductDetailDTO getProductDetails(long productId) {
-        // Fetch all async and combine the results without blocking
-        CompletableFuture<Product> productFuture = getProductById(productId);
-        CompletableFuture<Price> priceFuture = getPriceByProductById(productId);
-        CompletableFuture<Inventory> inventoryFuture = getInventoryByProductId(productId);
-
-        return CompletableFuture.allOf(productFuture, priceFuture, inventoryFuture)
-                .thenApply(voidResult -> {
-                    Product product = productFuture.join();
-                    Price price = priceFuture.join();
-                    Inventory inventory = inventoryFuture.join();
-
-                    // Build and return ProductDetailDTO
-                    return new ProductDetailDTO(
-                            productId,
-                            product.getCategory().getName(),
-                            product.getName(),
-                            product.getDescription(),
-                            inventory.getAvailableQuantity(),
-                            price.getPrice(),
-                            inventory.getStatus()
-                    );
-                }).join();
-    }
+    ProductDetailDTO getProductDetails(long productId);
 
 }
--- src/main/java/com/hemanttechie/facade/ProductSyncFacade.java
+++ src/main/java/com/hemanttechie/facade/ProductSyncFacade.java
@@ -1,44 +1,7 @@
 package com.hemanttechie.facade;
 
-
 import com.hemanttechie.dto.ProductDetailDTO;
-import com.hemanttechie.entity.Inventory;
-import com.hemanttechie.entity.Price;
-import com.hemanttechie.entity.Product;
-import com.hemanttechie.service.InventoryService;
-import com.hemanttechie.service.PriceService;
-import com.hemanttechie.service.ProductService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class ProductSyncFacade {
-
-    private final ProductService productService;
-    private final InventoryService inventoryService;
-    private final PriceService priceService;
-
-
-    public ProductDetailDTO getProductDetails(long productId) {
-        log.info("Sync facade for getting product details for the product id {}", productId);
-
-        //fetch product
-        Product product = productService.findById(productId);
-
-        //fetch the price
-        Price price = priceService.getPriceByProductId(productId);
-
-        //fetch the inventory
-        Inventory inventory = inventoryService.getInventoryByProductId(productId);
-
-        //combine result
-        return new ProductDetailDTO(productId, product.getCategory().getName(),
-                product.getName(), product.getDescription(),
-                inventory.getAvailableQuantity(), price.getPrice(),
-                inventory.getStatus());
-    }
-
+public interface ProductSyncFacade {
+    ProductDetailDTO getProductDetails(long productId);
 }
